57076fe0f1fd28f6843fb942d31703eb88762ca8
SakshamPokhrel175 <sakshampokhrel175@gmail.com>
2025-11-26 02:40:59 +0530

    Use CDN for Bootstrap; remove from angular.json to avoid CSS parser warnings and reduce bundle

---
+++ src/polyfills.ts
@@ -0,0 +1,13 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// Zone JS is required by Angular itself.
+import 'zone.js'; // Included with Angular CLI by default
+
+// If you use Angular Localize (i18n runtime)
+import '@angular/localize/init';
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
